1277240e15b140c90b5fd2ac9b2d4520ad374a0b
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-05-08 07:36:04 -0700

    Write unit tests for CategoryServiceImpl - Complete unit tests for getAll, getById, deleteById, existsByNameIgnoreCase, and getByType

--- src/test/java/com/stephenlindstrom/financeapp/budget_tool/service/CategoryServiceImplTest.java
+++ src/test/java/com/stephenlindstrom/financeapp/budget_tool/service/CategoryServiceImplTest.java
@@ -1,14 +1,17 @@
 package com.stephenlindstrom.financeapp.budget_tool.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -103,5 +106,141 @@ public class CategoryServiceImplTest {
     assertEquals(2, result.size());
     assertEquals("Rent", result.get(1).getName());
     assertEquals(TransactionType.EXPENSE, result.get(1).getType());
+  }
+
+  @Test 
+  void testGetAll_WithNoEntries_ReturnsEmptyList() {
+    // Arrange
+    List<Category> categoryList = new ArrayList<>();
+
+    when(categoryRepository.findAll()).thenReturn(categoryList);
+
+    // Act
+    List<CategoryDTO> result = categoryService.getAll();
+
+    // Assert
+    assertTrue(result.isEmpty());
+  }
+
+  @Test
+  void testGetById_IdExists_ReturnsOptionalCategoryDTO() {
+    // Arrange
+    Category savedCategory = Category.builder()
+            .id(1L)
+            .name("Groceries")
+            .type(TransactionType.EXPENSE)
+            .build();
+
+    when(categoryRepository.findById(1L)).thenReturn(Optional.of(savedCategory));
+
+    // Act
+    Optional<CategoryDTO> result = categoryService.getById(1L);
+    CategoryDTO dto = result.orElseThrow();
+
+    // Assert
+    assertEquals(1L, dto.getId());
+    assertEquals("Groceries", dto.getName());
+    assertEquals(TransactionType.EXPENSE, dto.getType());
+  }
+
+  @Test 
+  void testGetById_IdDoesNotExist_ReturnsOptionalEmpty() {
+    // Arrange
+    when(categoryRepository.findById(1L)).thenReturn(Optional.empty());
+
+    // Act
+    Optional<CategoryDTO> result = categoryService.getById(1L);
+
+    // Assert
+    assertTrue(result.isEmpty());
+  }
+
+  @Test
+  void testDeleteById_IdExists_NoReturnValue() {
+    // Act
+    categoryService.deleteById(1L);
+
+    // Assert
+    verify(categoryRepository).deleteById(1L);
+  }
+
+  @Test
+  void testExistsByNameIgnoreCase_NameExists_ReturnsTrue() {
+    // Arrange
+    when(categoryRepository.existsByNameIgnoreCase("Groceries")).thenReturn(true);
+
+    // Act and Assert
+    assertTrue(categoryService.existsByNameIgnoreCase("Groceries"));
+  }
+
+  @Test
+  void testExistsByNameIgnoreCase_NameDoesNotExist_ReturnsFalse() {
+    // Arrange
+    when(categoryRepository.existsByNameIgnoreCase("Groceries")).thenReturn(false);
+
+    // Act and Assert
+    assertFalse(categoryService.existsByNameIgnoreCase("Groceries"));
+  }
+
+  @Test
+  void testGetByType_WithMatchingEntries_ReturnsListOfCategoryDTO() {
+    // Arrange
+    Category savedCategory1 = Category.builder()
+            .id(1L)
+            .name("Groceries")
+            .type(TransactionType.EXPENSE)
+            .build();
+    
+    
+    Category savedCategory2 = Category.builder()
+            .id(2L)
+            .name("Rent")
+            .type(TransactionType.EXPENSE)
+            .build();
+
+    Category savedCategory3 = Category.builder()
+            .id(3L)
+            .name("Salary")
+            .type(TransactionType.INCOME)
+            .build();
+
+    List<Category> categoryList = new ArrayList<>(Arrays.asList(savedCategory1, savedCategory2, savedCategory3));
+
+    when(categoryRepository.findAll()).thenReturn(categoryList);
+
+    // Act
+    List<CategoryDTO> result = categoryService.getByType(TransactionType.EXPENSE);
+
+    // Assert
+    assertEquals(2, result.size());
+    assertEquals(TransactionType.EXPENSE, result.get(0).getType());
+    assertEquals(TransactionType.EXPENSE, result.get(1).getType());
+  }
+
+  @Test
+  void testGetByType_WithoutMatchingEntries_ReturnsEmptyList() {
+    // Arrange
+    Category savedCategory1 = Category.builder()
+            .id(1L)
+            .name("Groceries")
+            .type(TransactionType.EXPENSE)
+            .build();
+    
+    
+    Category savedCategory2 = Category.builder()
+            .id(2L)
+            .name("Rent")
+            .type(TransactionType.EXPENSE)
+            .build();
+
+    List<Category> categoryList = new ArrayList<>(Arrays.asList(savedCategory1, savedCategory2));
+
+    when(categoryRepository.findAll()).thenReturn(categoryList);
+
+    // Act
+    List<CategoryDTO> result = categoryService.getByType(TransactionType.INCOME);
+
+    // Assert
+    assertTrue(result.isEmpty());
   }
 }
