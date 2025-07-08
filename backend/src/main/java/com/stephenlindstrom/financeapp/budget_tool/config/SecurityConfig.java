51eb5c384cb2de010d391e573ef7cdc9c0740a58
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-07-08 07:35:06 -0700

    Add encoder to encrypt and decrypt passwords

--- backend/src/main/java/com/stephenlindstrom/financeapp/budget_tool/config/SecurityConfig.java
+++ backend/src/main/java/com/stephenlindstrom/financeapp/budget_tool/config/SecurityConfig.java
@@ -3,9 +3,13 @@ package com.stephenlindstrom.financeapp.budget_tool.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
   @Bean
@@ -17,5 +21,10 @@ public class SecurityConfig {
       );
 
     return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
