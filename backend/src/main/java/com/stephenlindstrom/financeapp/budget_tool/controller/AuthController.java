a17f1b64240b3e25ab24743da950640e7007787b
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-07-08 09:17:45 -0700

    Create login endpoint that authenticates user

--- backend/src/main/java/com/stephenlindstrom/financeapp/budget_tool/controller/AuthController.java
+++ backend/src/main/java/com/stephenlindstrom/financeapp/budget_tool/controller/AuthController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stephenlindstrom.financeapp.budget_tool.dto.LoginRequest;
 import com.stephenlindstrom.financeapp.budget_tool.model.User;
 import com.stephenlindstrom.financeapp.budget_tool.service.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -26,6 +29,16 @@ public class AuthController {
       return ResponseEntity.ok("User registered successfully");
     } catch (IllegalArgumentException e) {
       return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<String> login(@RequestBody @Valid LoginRequest loginRequest) {
+    try {
+        userService.authenticateUser(loginRequest.getUsername(), loginRequest.getPassword());
+        return ResponseEntity.ok("Login successful");
+    } catch (IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
   }
 }
