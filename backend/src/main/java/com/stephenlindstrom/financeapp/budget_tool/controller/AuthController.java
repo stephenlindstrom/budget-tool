2c0dd7f7280271c967078f5e2940f3c6af85ca27
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-07-22 09:45:37 -0700

    Generate JWT from username and return in response

--- backend/src/main/java/com/stephenlindstrom/financeapp/budget_tool/controller/AuthController.java
+++ backend/src/main/java/com/stephenlindstrom/financeapp/budget_tool/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.stephenlindstrom.financeapp.budget_tool.dto.LoginRequest;
 import com.stephenlindstrom.financeapp.budget_tool.dto.UserRegistrationDTO;
+import com.stephenlindstrom.financeapp.budget_tool.service.JwtService;
 import com.stephenlindstrom.financeapp.budget_tool.service.UserService;
 
 import jakarta.validation.Valid;
@@ -17,9 +18,11 @@ import jakarta.validation.Valid;
 public class AuthController {
 
   private final UserService userService;
+  private final JwtService jwtService;
 
-  public AuthController(UserService userService) {
+  public AuthController(UserService userService, JwtService jwtService) {
     this.userService = userService;
+    this.jwtService = jwtService;
   }
 
   @PostMapping("/register")
@@ -36,7 +39,8 @@ public class AuthController {
   public ResponseEntity<String> login(@RequestBody @Valid LoginRequest loginRequest) {
     try {
         userService.authenticateUser(loginRequest.getUsername(), loginRequest.getPassword());
-        return ResponseEntity.ok("Login successful");
+        String token = jwtService.generateToken(loginRequest.getUsername());
+        return ResponseEntity.ok(token);
     } catch (IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
