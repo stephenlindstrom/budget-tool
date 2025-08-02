fb92a0e6ecea02053851f77c87ee649cb8102590
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-08-02 09:49:28 -0700

    Add field-specific errors to ErrorResponse

--- backend/src/main/java/com/stephenlindstrom/financeapp/budget_tool/dto/ErrorResponse.java
+++ backend/src/main/java/com/stephenlindstrom/financeapp/budget_tool/dto/ErrorResponse.java
@@ -1,13 +1,27 @@
 package com.stephenlindstrom.financeapp.budget_tool.dto;
 
+import java.util.Map;
+
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 @Schema(description = "Error response object")
 public class ErrorResponse {
   @Schema(description = "Error message", example = "Resource not found")
   private String message;
+
+  @Schema(description = "Field-specific validation errors", example = "{\"username\": \"Username must not be blank\"}")
+  private Map<String, String> errors;
+
+  public ErrorResponse(String message) {
+    this.message = message;
+  }
+
+  public ErrorResponse(String message, Map<String, String> errors) {
+    this.message = message;
+    this.errors = errors;
+  }
 }
