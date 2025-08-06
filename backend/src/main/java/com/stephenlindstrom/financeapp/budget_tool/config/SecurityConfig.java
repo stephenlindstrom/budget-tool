397ab8aea8db47cc7d95c24cd9ae272ea47ac8be
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-08-06 09:57:02 -0700

    Add global CORS config to allow frontend requests

--- backend/src/main/java/com/stephenlindstrom/financeapp/budget_tool/config/SecurityConfig.java
+++ backend/src/main/java/com/stephenlindstrom/financeapp/budget_tool/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import com.stephenlindstrom.financeapp.budget_tool.security.JwtAuthFilter;
 import com.stephenlindstrom.financeapp.budget_tool.security.JwtAuthenticationEntryPoint;
 
@@ -34,6 +36,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
+      .cors(withDefaults())
       .csrf(csrf -> csrf.disable())
       .exceptionHandling(ex -> ex
         .authenticationEntryPoint(jwtAuthenticationEntryPoint))
