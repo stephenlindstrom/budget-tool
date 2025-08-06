fdc3baafc4096a5439cece15746934d2a1b37816
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-08-06 09:55:46 -0700

    Add LoginPage and configure routing

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api/api";
+import { useAuth } from "../hooks/useAuth";
+
+function LoginPage() {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({ username: "", password: "" });
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const response = await api.post("/auth/login", form);
+      login(response.data);
+      navigate("/dashboard");
+    } catch (err) {
+      console.error(err);
+      setError("Invalid username or password");
+    }
+  };
+
+  return (
+    <div style={{ maxWidth: 400, margin: "2rem auto"}}>
+      <h2>Login</h2>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label><br />
+          <input
+            type="text"
+            name="username"
+            value={form.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div style={{ marginTop: "1rem" }}>
+          <label>Password</label><br />
+          <input
+            type="password"
+            name="password"
+            value={form.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" style={{ marginTop: "1.5rem" }}>Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
