24e64e05f6b43b0aa30f5570a76eef4eb9db7a75
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-08-06 10:43:46 -0700

    Configure routing for Dashboard page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
     </Routes>
   );
 }
