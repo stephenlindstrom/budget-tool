12ed0f5e6f5c08c2f0527ed342b1257fc1ba781d
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-08-06 09:53:52 -0700

    Manage JWT with AuthContext and useAuth hook

---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { AuthContext } from "./AuthContext";
+
+export function AuthProvider({ children }) {
+  const [token, setToken] = useState(null);
+
+  const login = (newToken) => setToken(newToken);
+  const logout = () => setToken(null);
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
