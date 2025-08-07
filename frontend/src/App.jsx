5f28d8ceb775e2976e43f05d88199d61917b2c58
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-08-07 07:49:23 -0700

    Add route to create budget

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
 import BudgetSummaryPage from "./pages/BudgetSummaryPage";
+import CreateBudgetPage from "./pages/CreateBudgetPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<LoginPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/budgets/:value" element={<BudgetSummaryPage />} />
+      <Route path="/create_budget" element={<CreateBudgetPage />} />
     </Routes>
   );
 }
